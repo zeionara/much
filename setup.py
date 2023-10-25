6f5019d2c601273139dc071bc55ad9cdd7c58ac0
zeio <zeionara@gmail.com>
2023-10-25 15:14:53 +0300

    feat(release): increased current version, pushed to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='much',
-    version='0.0.3',
+    version='0.0.4',
     license='Apache 2.0',
     author='Zeio Nara',
     author_email='zeionara@gmail.com',
