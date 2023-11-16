395d1b80072ee887869f43a54a63cb3dbafc8359
Zeio Nara <zeionara@gmail.com>
2023-11-16 00:14:17 +0000

    fix(continue): updated continuation script considering output from the latest run

--- continue.sh
+++ continue.sh
@@ -2,4 +2,5 @@
 
 # python -m much filter -s 905500 -i assets/index.tsv -n 1000
 # python -m much filter -t 20 -s 863560 -i assets/index.tsv -n 1000
-python -m much filter -t 20 -s 804660 -i assets/index.tsv -n 100
+# python -m much filter -t 20 -s 804660 -i assets/index.tsv -n 100
+python -m much filter -t 20 -s 784940 -i assets/index.tsv -n 1000
