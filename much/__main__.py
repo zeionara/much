46ae9678ec282c5bad3c5f56da840b5dc943452a
Dmitrii <zeionara@gmail.com>
2024-03-22 14:05:56 +0300

    feat(image-uploader): added photo and video uploader

--- much/__main__.py
+++ much/__main__.py
@@ -40,6 +40,8 @@ from .nlp import summarize
 from .VkClient import VkClient
 from .VkFileUploader import VkFileUploader
 from .VkAudioUploader import VkAudioUploader
+from .VkPhotoUploader import VkPhotoUploader
+from .VkVideoUploader import VkVideoUploader
 from .ArtistSampler import ArtistSampler
 # from .HuggingFaceClient import HuggingFaceClient, Task
 # from .folder import cached_folder
@@ -222,6 +224,12 @@ def upload_file(path: str):
     # uploader.upload(path, title = 'Foo bar', tags = ['qux', 'quux'])
 
     VkAudioUploader.make().upload(path, title = 'Blah blah', artist = 'Foo bar')
+
+    # photo_id = VkPhotoUploader.make().upload(path, 'Foo bar baz', verbose = True)
+    # video_id = VkVideoUploader.make().upload(path, 'Foo bar baz', 'qux quux quuz', verbose = True)
+
+    # print(photo_id)
+    # print(video_id)
 
 
 @main.command()
