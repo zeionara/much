a1ec2d96da2181553b486305dfae99db357410d4
Zeio Nara <zeionara@gmail.com>
2026-01-04 16:56:22 +0300

    fix(envioronment): updated environmnent configuration files, updated script load.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-conda create -n much python=3.11.5
-conda activate much
+set -euo pipefail
 
-conda install requests pandas click tqdm python-lsp-server beautifulsoup4
+conda create -n much 'python<3.13' -y
+
+conda run -n much --no-capture-output pip install requests
+conda run -n much --no-capture-output pip install beautifulsoup4
+conda run -n much --no-capture-output pip install numpy
+conda run -n much --no-capture-output pip install click
+conda run -n much --no-capture-output pip install pandas
+conda run -n much --no-capture-output pip install tqdm
+conda run -n much --no-capture-output pip install flask
+conda run -n much --no-capture-output pip install torch torchvision torchaudio
+conda run -n much --no-capture-output pip install scipy
+conda run -n much --no-capture-output pip install ipython
+conda run -n much --no-capture-output pip install pydub
+# conda run -n much --no-capture-output pip install audioop-lts
+conda run -n much --no-capture-output pip install music-tag
+conda run -n much --no-capture-output pip install num2words
+conda run -n much --no-capture-output pip install transliterate
+conda run -n much --no-capture-output pip install kokoro
+conda run -n much --no-capture-output pip install google-images-search
+
+sudo bash -c 'apt-get update && apt-get install ffmpeg -y'
