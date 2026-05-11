76df9648e436696950cb6e882a4bab7d1c6f0e57
Zeio Nara <zeionara@gmail.com>
2026-05-11 17:41:53 +0300

    feat(requirements-cpu): added requirements-cpu, updated load.sh to use venv instead of conda

--- load.sh
+++ load.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 PROJECT_ROOT=/opt/much
-CONDA_ROOT=/opt/conda
+VENV_ROOT=/opt/marude/.venv
 
 DATASET_ROOT=$PROJECT_ROOT/assets/patch
 
@@ -17,10 +17,9 @@ if test ! -d $LOG_ROOT; then
 fi
 
 if test ! -d $DATASET_ROOT; then
-  mkdir $DATASET_ROOT
+  echo 'Dataset root folder does not exist'
+  exit 1
 fi
-
-. "$CONDA_ROOT/etc/profile.d/conda.sh"
 
 if test -f $LOG_FILE; then
   echo >> $LOG_FILE
@@ -29,4 +28,4 @@ fi
 date +"%Y-%m-%d %H:%M:%S" >> $LOG_FILE
 
 cd $PROJECT_ROOT
-conda run -n raconteur --no-capture-output python -m much load -i $DATASET_ROOT/index.tsv -r $DATASET_ROOT/media -p $DATASET_ROOT/threads 1>> $LOG_FILE 2>&1
+$VENV_ROOT/bin/python -m much load -i $DATASET_ROOT/index.tsv -r $DATASET_ROOT/media -p $DATASET_ROOT/threads 1>> $LOG_FILE 2>&1
